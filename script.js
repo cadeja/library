3bada782790d1fc4488a8edb2d7bb804a41262bf
Cade <cjayenn@gmail.com>
2022-06-23 15:45:09 -0400

    added remove button. It's currently buggy

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 
 let myLibrary = [];
 
+let bookID = 0;
 
-function Book(title, author, haveRead){
+function Book(title, author, haveRead, id){
   this.title = title;
   this.author = author;
   this.haveRead = haveRead;
@@ -13,6 +14,9 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+Book.prototype.switchReadStatus = function() {
+  this.haveRead = this.haveRead == true ? false : true;
+}
 
 // WEB INTERFACE
 
@@ -22,14 +26,30 @@ const bookshelf = document.getElementById('bookshelf');
 function displayBooks() {
   bookshelf.innerHTML = '';
   for (let i = 0; i < myLibrary.length; i++){
+
     const div = document.createElement('div');
     div.setAttribute('class','book');
+    div.setAttribute('id', `book${i}`);
     div.innerHTML = `<div class="book-title">${myLibrary[i].title}</div>`
     div.innerHTML += `<div class="book-author">${myLibrary[i].author}</div>`
     div.innerHTML += `<div class="book-read-status">${myLibrary[i].haveRead}</div>`
+    div.innerHTML += `<div class="toggle-read-status"><button id="remove-book-${i}">x</button></div>`;
     bookshelf.appendChild(div);
+
+    //Delete Button
+    const removeBook = document.getElementById(`remove-book-${i}`);
+    removeBook.addEventListener('click', () => {
+      let id = removeBook.id[removeBook.id.length - 1]
+      console.log(id);
+      myLibrary.splice(id, id+1);
+      displayBooks();
+      
+
+    });
   }
 }
+
+
 
 //initialize bookshelf display
 displayBooks();
@@ -60,13 +80,16 @@ window.onclick = function(event) {
 
 // Form Submission
 
+const form = document.getElementById('newBookForm');
 const newBookBtn = document.getElementById('newBookBtn');
+//form.addEventListener('submit', (event) => {
 newBookBtn.addEventListener('click', () => {
-  const form = document.getElementById('newBookForm');
+  
   let title = form.elements['bookTitle'].value;
   let author = form.elements['bookAuthor'].value;
   let haveRead = form.elements['read'].checked ? true : false;
   addBookToLibrary(new Book(title, author, haveRead));
 
+  modal.style.display = 'none';
   displayBooks();
 });
